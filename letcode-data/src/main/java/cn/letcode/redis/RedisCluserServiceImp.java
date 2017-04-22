a44fe8d55c25cf90d82000f3dbb5af298c250858
chenshuaijun <chensj@letcode.cn>
2017-04-22 10:41:52 +0800

    修改注释

---
+++ letcode-data/src/main/java/cn/letcode/redis/RedisCluserServiceImp.java
@@ -0,0 +1,12 @@
+package cn.letcode.redis;
+
+/**
+ * Created by chenshuaijun on 2017/4/22.
+ * redis集群配置
+ *
+ * @author chenshuaijun
+ */
+public class RedisCluserServiceImp {
+    // TODO 处理redis集群配置读取方案
+
+}
