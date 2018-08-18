1b3849b98d4c4667810a7cc03aacf393879a6458
chenshuaijun <chensj@letcode.cn>
2018-08-18 17:31:13 +0800

    修改 xml 数据转换 处理性能

--- letcode-utils/src/main/java/cn/letcode/utils/xml/XmlUtils.java
+++ letcode-utils/src/main/java/cn/letcode/utils/xml/XmlUtils.java
@@ -12,144 +12,156 @@ import org.dom4j.io.XMLWriter;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by chenshuaijun on 2017/4/21. XML报文处理工具
  */
 public class XmlUtils {
-	/**
-	 * 将实体bean转成XML字符串
-	 *
-	 * @param object
-	 *            bean对象
-	 * @return xmlstring
-	 */
-	public static String beanToString(Object object) {
-		// 初始化处理实例
-		XStream xstream = new XStream(new XppDriver(new XmlFriendlyNameCoder("_-", "_")));
-		xstream.autodetectAnnotations(true);
-		// 设置处理过程中，忽略未知的元素
-		xstream.ignoreUnknownElements();
-		return formatXMLInOneLine(appendHeadXml(xstream.toXML(object)));
-	}
 
-	/**
-	 * 将xml字符串 转成bean
-	 *
-	 * @param xmlStr
-	 *            xml字符串
-	 * @param bean
-	 *            需要转换的对象实例
-	 * @param rootName
-	 *            xml元素root跟名称
-	 * @return bean对象
-	 * @author chenshuaijun
-	 */
-	public static Object xmlStrToBean(String xmlStr, Class<?> bean, String rootName) {
-		// 初始化处理实例
-		XStream xstream = new XStream(new XppDriver(new XmlFriendlyNameCoder("_-", "_")));
-		xstream.autodetectAnnotations(true);
-		// 设置处理过程中，忽略未知的元素
-		xstream.ignoreUnknownElements();
-		xstream.alias(rootName, bean);
-		return xstream.fromXML(xmlStr);
-	}
+    private static ConcurrentMap<String, XStream> xStreamMap = new ConcurrentHashMap();
 
-	/**
-	 * 美化xml内容，格式化
-	 *
-	 * @param inputXML
-	 *            输入需要格式化的xml字符串
-	 * @return 返回格式化后的内容
-	 * @throws Exception
-	 *             处理异常
-	 * @author chenshuaijun
-	 */
-	public static String formatXML(String inputXML) throws Exception {
-		SAXReader reader = new SAXReader();
-		Document document = reader.read(new StringReader(inputXML));
-		String retXML = null;
-		XMLWriter writer = null;
-		if (document != null) {
-			try {
-				StringWriter stringWriter = new StringWriter();
-				OutputFormat format = new OutputFormat(" ", true);
-				writer = new XMLWriter(stringWriter, format);
-				writer.write(document);
-				writer.flush();
-				retXML = stringWriter.getBuffer().toString();
-			} finally {
-				if (writer != null) {
-					try {
-						writer.close();
-					} catch (IOException e) {
-					}
-				}
-			}
-		}
-		return retXML;
-	}
+    /**
+     * 将实体bean转成XML字符串
+     *
+     * @param object bean对象
+     * @return xmlstring
+     */
+    public static String beanToString(Object object) {
+        // 初始化处理实例
+        XStream xstream = getXStreamBean(object.getClass().getName());
+        xstream.autodetectAnnotations(true);
+        // 设置处理过程中，忽略未知的元素
+        xstream.ignoreUnknownElements();
+        return formatXMLInOneLine(appendHeadXml(xstream.toXML(object)));
+    }
 
-	/**
-	 * 将xml转换成功一行
-	 *
-	 * @param inputXML
-	 *            输入xml内容
-	 * @return 格式化后的内容
-	 * @author chenshuaijun
-	 */
-	public static String formatXMLInOneLine(String inputXML) {
-		String retXml = "";
-		XMLWriter writer = null;
-		try {
-			SAXReader reader = new SAXReader();
-			Document document = reader.read(new StringReader(inputXML));
-			if (document != null) {
-				StringWriter stringWriter = new StringWriter();
-				OutputFormat format = new OutputFormat("", false);
-				format.setTrimText(true);
-				format.setNewLineAfterDeclaration(false);
-				writer = new XMLWriter(stringWriter, format);
-				writer.write(document);
-				writer.flush();
-				retXml = stringWriter.getBuffer().toString();
-			}
-		} catch (IOException | DocumentException e) {
-			return "";
-		} finally {
-			if (writer != null) {
-				try {
-					writer.close();
-				} catch (IOException e) {
-				}
-			}
-		}
+    /**
+     * 将xml字符串 转成bean
+     *
+     * @param xmlStr   xml字符串
+     * @param bean     需要转换的对象实例
+     * @param rootName xml元素root跟名称
+     * @return bean对象
+     * @author chenshuaijun
+     */
+    public static Object xmlStrToBean(String xmlStr, Class<?> bean, String rootName) {
+        // 初始化处理实例
+        XStream xstream = getXStreamBean(bean.getClass().getName());
+        xstream.autodetectAnnotations(true);
+        // 设置处理过程中，忽略未知的元素
+        xstream.ignoreUnknownElements();
+        xstream.alias(rootName, bean);
+        return xstream.fromXML(xmlStr);
+    }
 
-		return retXml;
-	}
+    /**
+     * 从内存中获取实例对象，增加处理效率
+     *
+     * @param name
+     * @return
+     */
+    public static XStream getXStreamBean(String name) {
+        XStream xstream = xStreamMap.get(name);
+        if (xstream == null) {
+            xstream = new XStream(new XppDriver(new XmlFriendlyNameCoder("_-", "_")));
+            xStreamMap.put(name, xstream);
+        }
+        return xstream;
+    }
 
-	/**
-	 * 拼接xml报文头部分<br>
-	 * 默认编码 UTF-8
-	 *
-	 * @param xml
-	 *            头部分内容
-	 * @return 添加xml头后的字符串
-	 */
-	public static String appendHeadXml(String xml) {
-		return appendHeadXml(xml, "utf-8");
-	}
 
-	/**
-	 * 拼接xml报文头部分
-	 *
-	 * @param xml
-	 *            XML字符串
-	 * @param charset
-	 *            指定编码集
-	 * @return 返回添加后的字符串
-	 */
-	public static String appendHeadXml(String xml, String charset) {
-		return "<?xml version=\"1.0\" encoding=\"" + charset + "\"?>\n" + (xml == null ? "" : xml);
-	}
+    /**
+     * 美化xml内容，格式化
+     *
+     * @param inputXML 输入需要格式化的xml字符串
+     * @return 返回格式化后的内容
+     * @throws Exception 处理异常
+     * @author chenshuaijun
+     */
+    public static String formatXML(String inputXML) throws Exception {
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(new StringReader(inputXML));
+        String retXML = null;
+        XMLWriter writer = null;
+        if (document != null) {
+            try {
+                StringWriter stringWriter = new StringWriter();
+                OutputFormat format = new OutputFormat(" ", true);
+                writer = new XMLWriter(stringWriter, format);
+                writer.write(document);
+                writer.flush();
+                retXML = stringWriter.getBuffer().toString();
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        }
+        return retXML;
+    }
+
+    /**
+     * 将xml转换成功一行
+     *
+     * @param inputXML 输入xml内容
+     * @return 格式化后的内容
+     * @author chenshuaijun
+     */
+    public static String formatXMLInOneLine(String inputXML) {
+        String retXml = "";
+        XMLWriter writer = null;
+        try {
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(new StringReader(inputXML));
+            if (document != null) {
+                StringWriter stringWriter = new StringWriter();
+                OutputFormat format = new OutputFormat("", false);
+                format.setTrimText(true);
+                format.setNewLineAfterDeclaration(false);
+                writer = new XMLWriter(stringWriter, format);
+                writer.write(document);
+                writer.flush();
+                retXml = stringWriter.getBuffer().toString();
+            }
+        } catch (IOException | DocumentException e) {
+            return "";
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        return retXml;
+    }
+
+    /**
+     * 拼接xml报文头部分<br>
+     * 默认编码 UTF-8
+     *
+     * @param xml 头部分内容
+     * @return 添加xml头后的字符串
+     */
+    public static String appendHeadXml(String xml) {
+        return appendHeadXml(xml, "utf-8");
+    }
+
+    /**
+     * 拼接xml报文头部分
+     *
+     * @param xml     XML字符串
+     * @param charset 指定编码集
+     * @return 返回添加后的字符串
+     */
+    public static String appendHeadXml(String xml, String charset) {
+        return "<?xml version=\"1.0\" encoding=\"" + charset + "\"?>\n" + (xml == null ? "" : xml);
+    }
 }
